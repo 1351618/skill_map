a2a0c4a43aa232521e1b88984fe9afb54ad2c088
1351618 <1351618@gmail.com>
2024-03-21 00:12:40 +0200

    chore: btn settings

--- src/shared/ui/PageWrapper/PageWrapper.tsx
+++ src/shared/ui/PageWrapper/PageWrapper.tsx
@@ -1,11 +1,13 @@
 import cls from "./pageWrapper.module.scss";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { PersonalArea } from "../../../widgets/personalArea/ui/PersonalArea";
 import { MiniNavBar } from "../../../widgets/miniNavBar/ui/MiniNavBar";
 import { BtnShowHideInterface } from "../../../widgets/btnShowHideInterface/ui/btnShowHideInterface";
 import { BoxSettings } from "../../../widgets/boxSettings/ui/BoxSettings";
+import settingsSvg from "./settings.svg";
 
 export const PageWrapper = ({ children }: { children: ReactNode }) => {
+  const [isSettingsShow, setSettingsShow] = useState(false);
   return (
     <div className={cls.app}>
       <section className={cls.sectionPersonalArea}>
@@ -18,7 +20,18 @@ export const PageWrapper = ({ children }: { children: ReactNode }) => {
       <main className="main">
         <div className="Home__inner">{children}</div>
       </main>
-      <section className={cls.sectionBoxSettings}>
+      <section
+        className={`${cls.sectionBoxSettings} ${
+          isSettingsShow ? cls.show : ""
+        }`}
+      >
+        <button
+          onClick={() => {
+            setSettingsShow(!isSettingsShow);
+          }}
+        >
+          <img className={cls.settingsSvg} src={settingsSvg} alt="" />
+        </button>
         <BoxSettings />
       </section>
       <section className={cls.sectionBtnShowHideInterface}>
